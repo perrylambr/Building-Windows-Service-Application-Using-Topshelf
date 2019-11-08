44210d2f102eef72abfbdbe652e49b1218694616
Perry Lambrechts <p.lambrechts@ettubv.onmicrosoft.com>
2019-11-08 17:15:56 +0100

    Initial commit

---
+++ TopshelfDemo/MyWindowsService.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+
+namespace TopshelfDemo
+{
+    public class MyWindowsService
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(MyWindowsService));
+
+        public void Start()
+        {
+            _log.Info("Starting...");
+            // business logic goes here
+            _log.Info("Started succesfully.");
+        }
+
+        public void Stop()
+        {
+            _log.Info("Stopping...");
+            // business logic goes here
+            _log.Info("Stopped succesfully.");
+        }
+        public void Pause()
+        {
+        }
+
+        public void Resume()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
---
+++ TopshelfDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Reflection;
+using Topshelf;
+
+namespace TopshelfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+                XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+                hostConfig.UseLog4Net();
+
+                hostConfig.EnableServiceRecovery(serviceRecovery =>
+                {
+                    // first failure, 5 minute delay
+                    serviceRecovery.RestartService(5);
+
+                    // second failure, 10 minute delay
+                    serviceRecovery.RunProgram(10, @"C:\Windows\Notepad.exe");
+
+                    // subsequent failures, 15 minute delay
+                    serviceRecovery.RestartComputer(15, "Topshelf demo failure");
+                });
+
+                hostConfig.DependsOn("Spooler");
+                hostConfig.DependsOnEventLog();
+
+                hostConfig.UseAssemblyInfoForServiceInfo();
+
+                hostConfig.StartManually();
+
+                hostConfig.RunAsNetworkService();
+
+                hostConfig.Service<MyWindowsService>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new MyWindowsService());
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+
+                    serviceConfig.WhenPaused(s => s.Pause());
+                    serviceConfig.WhenContinued(s => s.Resume());
+                    serviceConfig.WhenShutdown(s => s.Shutdown());
+                });
+            });
+        }
+    }
+}
